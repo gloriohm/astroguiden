99a2b220f4a55849b5d407f3f8f7ebaed960005c
mangum0pus <138219079+mangum0pus@users.noreply.github.com>
2023-09-20 15:47:05 +0200

    turned off ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,4 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  output: "server",
-  adapter: vercel()
 });
